b36cfcbf969720c0d0ba6b88e2e76dc27613681e
CocaW <williamcoca35@gmail.com>
2022-06-02 11:57:26 -0300

    Se agrega el proyecto

---
+++ menu.js.js
@@ -0,0 +1,7 @@
+const navegacion = document.querySelector(".navegacion");
+const menu = document.querySelector(".menu-navegacion");
+console.log(navegacion)
+console.log(menu)
+navegacion.addEventListener("click", () =>{
+    menu.classList.toggle("spread")
+})
